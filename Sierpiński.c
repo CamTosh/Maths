6502e445c9e73379d7ecb37fd79ec54d331303c7
Tosh Camille <tochecamille@gmail.com>
2015-12-17 10:58:40 +0100

    Ajout de couleurs et des numéros de ligne
    Pair : Cyan
    Impair : Rouge
    
    Numéro de ligne -> jusqu'à 99

--- Sierpiński.c
+++ Sierpiński.c
@@ -18,20 +18,29 @@ int main() {
     printf("Entrez la taille du triangle : ");
     scanf("%d", &taille);
 
-    for (i = 0 ;i < taille; i++) {
+    for (i = 0 ; i < taille; i++) {
+        if(i < 10){
+            Color(7, 0); //blanc
+            printf("%d  ", i);
+        }
+        else {
+            Color(7, 0); //blanc
+            printf("%d ",i);
+        }
         for (j = taille - i; j > 0; j--) {
             printf(" ");
         }   
         for (k = 0 ; k < 1 + 2 * i; k++) {
             if ((k % 2) == 0) {
-                Color(4, 0);
+                Color(3, 0); // cyan
                 printf("*");
             }
             else if((k % 2) != 2) {
-                Color(2, 0);
+                Color(4, 0); // rouge
                 printf("*");
             }
         }
+        Color(7, 0); //blanc
         printf("\n");
     }
 return 0;
